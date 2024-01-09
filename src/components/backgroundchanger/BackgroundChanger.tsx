56ea1be63c50246fee7144b2016f29d2c6b59d00
bijayneupane18 <bjneupane5@gmail.com>
2024-01-09 17:42:34 +0545

    completeed: background color changer

--- src/components/backgroundchanger/BackgroundChanger.tsx
+++ src/components/backgroundchanger/BackgroundChanger.tsx
@@ -1,15 +1,20 @@
-import Button from "./button/Button"
-import Input from "./input/Input"
+import { useState } from "react";
+import Input from "./input/Input";
+
 
 const BackgroundChanger = () => {
+
+    const[bgColor, setBgColor] = useState("");  
+    
   return (
     <>
-     <div>
-        <Input/>
-        <Button/>
-     </div> 
+      <div className={`flex flex-col justify-center items-center gap-5 m-auto h-screen`}
+      style={{backgroundColor:`${bgColor}`}}
+      >
+        <Input setBgColor = {setBgColor}/>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default BackgroundChanger
+export default BackgroundChanger;
--- src/components/backgroundchanger/input/Input.tsx
+++ src/components/backgroundchanger/input/Input.tsx
@@ -1,10 +1,24 @@
 
-const Input = () => {
-  return (
-    <>
-      <div>this is input</div>
-    </>
-  )
+interface colorProps {
+  setBgColor: (st:string) => void
 }
 
-export default Input
+const Input = ({setBgColor}:colorProps) => {
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setBgColor(e.target.value);
+  }
+  
+  return (
+    <>
+      <input
+        className={`border border-solid border-slate-500 px-3 py-1 focus:outline-slate-500`}
+        type="text"
+        onChange={handleChange}
+        placeholder="Enter Color"
+      />
+    </>
+  );
+};
+
+export default Input;
